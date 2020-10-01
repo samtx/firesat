2d5a7392af6655010f10d3e6ebe24aaa10733a05
Sam Friedman <samtx@outlook.com>
2020-10-01 15:02:04 -0500

    Use newer, faster version of python-sgp4

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ description = (
     "described in Wertz 'Space Mission Analysis and Design', 1999"
 )
 
-ext = cythonize(['firesat/_sgp4.pyx'], language_level = 3)
-
+# ext = cythonize(['firesat/_sgp4.pyx'], language_level = 3)
+ext = []
 
 # ext = [Extension('sgp4lib',
 #                 sources=['firesat/sgp4lib.pyx', 'firesat/SGP4.cpp'],
@@ -30,6 +30,7 @@ setup(
     python_requires='>=3',
     install_requires=[
         'numpy>=1.14',
+        'sgp4>=2.8',
     ],
     test_suite='nose.collector',
     tests_require=[
